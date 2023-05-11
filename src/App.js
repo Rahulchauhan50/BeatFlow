55ce9c262b76565f199e513d45dccaf62a270ab9
Rahulchauhan50 <130024449+Rahulchauhan50@users.noreply.github.com>
2023-05-11 16:04:01 +0000

    initial

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import Discover from './components/Discover';
+import Sidebar from './components/Sidebar';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Router>
+      <div className="relative flex">
+      <Sidebar />
+      <div className="flex-1 flex flex-col bg-gradient-to-br from-black to-[#121286]">
+
+        <div className="px-6 h-[calc(100vh-72px)] overflow-y-scroll hide-scrollbar flex xl:flex-row flex-col-reverse">
+          <div className="flex-1 h-fit pb-40">
+            <Routes>
+              <Route exact path="/" element={<Discover />} />
+            </Routes>
+          </div>
+          <div className="xl:sticky relative top-0 h-fit">
+          </div>
+        </div>
+      </div>
+
+        {/* <div className="absolute h-28 bottom-0 left-0 right-0 flex animate-slideup bg-gradient-to-br from-white/10 to-[#2a2a80] backdrop-blur-lg rounded-t-3xl z-10"></div> */}
     </div>
+    </Router>
   );
 }
 
