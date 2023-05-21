b7329738ad8244a3321b40dc7f695ce3d7c37583
Rahulchauhan50 <130024449+Rahulchauhan50@users.noreply.github.com>
2023-05-21 12:57:27 +0000

    test

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -7,7 +7,7 @@ export default function Loader() {
        <div>
        <img src={loader} alt='Loader' className='w-32 h-32 object-contain'/>
         <h1 className='font-bold text-2xl text-white mt-2'>
-          Loading songs...
+          Loading songs...  
         </h1>
        </div>
          
