7b43740378d0ef5e4c1331a4ed03e7bec4afe70e
Rahul chauhan <rahulchauhah50@gmail.com>
2023-07-29 08:27:06 +0530

    h

--- src/redux/services/UserApi.js
+++ src/redux/services/UserApi.js
@@ -1,10 +1,10 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
-const BaseUrl = process.env.REACT_APP_BASE_URL
+// const BaseUrl = process.env.REACT_APP_BASE_URL
 
 export const UserDataApi = createApi({
   reducerPath: 'UserDataApi',
   baseQuery: fetchBaseQuery({
-    baseUrl: BaseUrl,
+    baseUrl: 'https://music-yevp.onrender.com/',
     prepareHeaders: (headers) => {
       headers.set('auth-token', localStorage.getItem("token")) 
       headers.set('Content-Type', 'application/json')
@@ -94,7 +94,7 @@ export const UserDataApi = createApi({
   export const UserAuthApi = createApi({
     reducerPath: 'UserAuthApi',
     baseQuery: fetchBaseQuery({
-      baseUrl: BaseUrl,
+      baseUrl: 'https://music-yevp.onrender.com/',
       prepareHeaders: (headers) => {
         headers.set('auth-token', localStorage.getItem('token')) 
         headers.set('Content-Type', 'application/json')
